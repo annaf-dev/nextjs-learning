8f3ff6e297725bb2cde807ebb444d01c77313174
Anna Fischer <deathangeldaemon@gmail.com>
2022-03-31 14:08:47 +0200

    build up new clean start project

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,14 +1,5 @@
-import Todo from "./components/Todo";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo title="Learn React" />
-      <Todo title="Master React" />
-      <Todo title="Explore React" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
