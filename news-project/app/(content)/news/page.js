6e7bdd89e1af5beb212fb187a4cedfa02c5e3b45
Anna Fischer <deathangeldaemon@gmail.com>
2024-12-16 14:09:52 +0100

    update static news fetching to database usage

--- news-project/app/(content)/news/page.js
+++ news-project/app/(content)/news/page.js
@@ -1,11 +1,13 @@
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news/news-list";
+import { getAllNews } from "@/lib/news";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+  const news = await getAllNews();
+
   return (
     <div id="news">
       <h1>News Page List</h1>
-      <NewsList news={DUMMY_NEWS}/>
+      <NewsList news={news}/>
     </div>
   );
 }
